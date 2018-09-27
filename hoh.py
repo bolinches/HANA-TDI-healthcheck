271d8e04418b6c513bf5d168ad6fa9aed8f39dea
Luis Bolinches <luis.bolinches@fi.ibm.com>
2018-09-27 17:55:03 +0200

    multipath_checker 3

--- hoh.py
+++ hoh.py
@@ -380,24 +380,41 @@ def ibm_power_package_check(ibm_power_packages_dictionary):
 def multipath_checker(svc_multipath_dictionary,mp_conf_dictionary):
 
     mp_errors = 0
-    for mp_value in svc_multipath_dictionary.keys():
+    for mp_attr in svc_multipath_dictionary.keys():
+        mp_value = svc_multipath_dictionary[mp_attr]
         #We go to check each entry on the JSON to both defaults and devices
         #We assume only defaults or devices contains the configuration for multipath
         #Lets look at defaults first if what we look is not there or worng value mark errors
         #If does not exist we move to look into devices 2145
-        for item in mp_conf_dictionary:
-            if 'defaults' in item:
-                for value in item['defaults']:
-                    if mp_value in value: #We found the value!
-                        recommended_value = svc_multipath_dictionary[mp_value]
-                        current_value = value[mp_value]
-                        if recommended_value == current_value:
-                            print(GREEN + "OK: " + NOCOLOR + mp_value + " has the recommended value of " + str(recommended_value))
+        if mp_attr == 'json_version': #Ignore JSON version
+            continue
+
+        if mp_attr == 'fast_io_fail_tmo':
+            for item in mp_conf_dictionary:
+                if default in item['defaults']:
+                    if mp_attr in default:
+                        print (GREEN + "OK: " + NOCOLOR + mp_attr + "found in defaults")
+                        current_value = default['mp_attr']
+                        if current_value == mp_value:
+                            print(GREEN + "OK: " + NOCOLOR + mp_attr + " has the recommended value of " + str(mp_value))
                         else:
-                            print (RED + "ERROR: " + NOCOLOR + sysctl + " is " + current_value_str + " and should be " + recommended_value_str)
-                            mp_errors = mp_errors + 1
+                            print (RED + "ERROR: " + NOCOLOR + mp_attr + " is " + str(current_value) + " and should be " + str(recommended_value)
                     else:
-                        print(YELLOW + "WARNING: " + NOCOLOR + mp_value + " not found in defaults")
+                    print(YELLOW + "WARNING: " + NOCOLOR + mp_attr + " not found in defaults")
+        #for item in mp_conf_dictionary:
+        #    if 'defaults' in item:
+        #        for value in item['defaults']:
+        #            recommended_value = svc_multipath_dictionary[mp_value]
+        #            print (str(recommended_value))
+        #            current_value = value[mp_value]
+        #            print(str(current_value))
+        #            if recommended_value == current_value:
+        #                print(GREEN + "OK: " + NOCOLOR + mp_value + " has the recommended value of " + str(recommended_value))
+        #            else:
+        #                print (RED + "ERROR: " + NOCOLOR + mp_value + " is " + str(current_value) + " and should be " + str(recommended_value)
+        #                mp_errors = mp_errors + 1
+        #    else:
+        #            print(YELLOW + "WARNING: " + NOCOLOR + mp_value + " not found in defaults")
     return mp_errors
 
 def load_multipath(multipath_file):
